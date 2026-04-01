9cc86980587cb173f04897fee09fcd4c5fbaaa8f
Tatsuya Ishii <tishii2479@gmail.com>
2026-04-01 23:34:27 +0900

    wip

--- autotune/README.md
+++ autotune/README.md
@@ -1,10 +1,1 @@
 # Autotune: Automatically tune hyperparameters for Atcoder Heuristic Contest
-
-## 1. 準備
-
-```bash
-pip install optuna
-cargo install pahcer
-uv tool install --no-cache ahc-utils --force
-cargo install --path .
-```
--- autotune/example/test.sh
+++ autotune/example/test.sh
@@ -1,7 +1,15 @@
-# uv tool install --no-cache ../../ahc-utils --force
+set -euo pipefail
+
+STUDY_PREFIX="example_$(date +%s)"
+echo "Using study prefix: ${STUDY_PREFIX}"
+
+pip install optuna
+cargo install pahcer
+uv tool install --no-cache ../../ahc-utils --force
+cargo install --path ..
 
 # 1. params_impl!を生成する
-# generate-impl --optuna_config_path optuna_config.yaml
+generate_impl --optuna_config_path optuna_config.yaml
 
 # 2. 生成されたコードをexample/src/main.rsにコピーして、パラメータの定義を行う
 # params_impl! {
@@ -10,14 +18,13 @@
 # }
 
 # pahcer-optunaを使う場合
-# pahcer-optuna --study_name study-optuna0 --optuna_config_path optuna_config.yaml
-# generate-impl --optuna_config_path optuna_config.yaml --study_name study-optuna0
+pahcer-optuna --study_name ${STUDY_PREFIX}_p --optuna_config_path optuna_config.yaml
+generate_impl --optuna_config_path optuna_config.yaml --study_name ${STUDY_PREFIX}_p
 
-autotune --config_path autotune_config.yaml --optuna_study_prefix test_study
+autotune --config_path autotune_config.yaml --optuna_study_prefix ${STUDY_PREFIX}
 
-optuna best-trial --study-name test_study_0 --storage sqlite:///optuna.db -f json
-optuna best-trial --study-name test_study_1 --storage sqlite:///optuna.db -f json
-optuna best-trial --study-name test_study_2 --storage sqlite:///optuna.db -f json
-optuna best-trial --study-name test_study_3 --storage sqlite:///optuna.db -f json
-
-pahcer-optuna --study_name study-optuna0 --optuna_config_path optuna_config.yaml --pahcer_config_path autotune/0/pahcer_config.toml
+optuna best-trial --study-name ${STUDY_PREFIX}_p --storage sqlite:///optuna.db -f json
+optuna best-trial --study-name ${STUDY_PREFIX}_0 --storage sqlite:///optuna.db -f json
+optuna best-trial --study-name ${STUDY_PREFIX}_1 --storage sqlite:///optuna.db -f json
+optuna best-trial --study-name ${STUDY_PREFIX}_2 --storage sqlite:///optuna.db -f json
+optuna best-trial --study-name ${STUDY_PREFIX}_3 --storage sqlite:///optuna.db -f json
